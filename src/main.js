e7838f5dc4370362f60c1dd421f37b4c78323ad4
wanggeng <18367319791@163.com>
2023-05-19 10:46:32 +0800

    feat:饿了么组件库使用中文

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+// 饿了么组件库使用中文
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+// 全局样式、全局字体相关
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -10,5 +15,12 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// 使用element-plus 并且设置全局的大小
+app.use(ElementPlus, {
+  locale: zhCn,
+  // 支持 large、default、small
+  size: Cookies.get('size') || 'default'
+})
 
 app.mount('#app')
